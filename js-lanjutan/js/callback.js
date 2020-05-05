678c104774767996997fe02c8ef19a09d7a5ccb3
nenfie <nenfie@gmail.com>
2020-05-06 00:07:04 +0700

    update: folder js-lanjutan

--- js-lanjutan/js/callback.js
+++ js-lanjutan/js/callback.js
@@ -1,1 +1,40 @@
 // Callback
+
+// 1. Synchronous Callback
+// function halo(nama) {
+//   alert(`Halo ${nama}`);
+// }
+
+// function tampilkanPesan(callback) {
+//   const nama = prompt("Masukkan Nama : ");
+//   callback(nama);
+// }
+
+// tampilkanPesan(function (nama) {
+//   return alert(`Halo ${nama}`);
+// });
+
+// 2. Asynchronous Callback
+const mhs = [
+  {
+    nama: "Sandhika Galih",
+    nrp: "04323423",
+    email: "sandhika@email.com",
+    jurusan: "Teknik Informatika",
+    idDosenWali: 1,
+  },
+  {
+    nama: "Doddy Ferdiansyah",
+    nrp: "13304343",
+    email: "doddy@email.com",
+    jurusan: "Teknik Informatika",
+    idDosenWali: 2,
+  },
+  {
+    nama: "Erik",
+    nrp: "10433623",
+    email: "erik@email.com",
+    jurusan: "Teknik Industri",
+    idDosenWali: 2,
+  },
+];
